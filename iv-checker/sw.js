a8fda0904e3ef4b1d4a6b78c1142527ab80216f2
TARCY <subarunohoshi33@gmail.com>
2026-07-19 01:48:32 +0900

    Add files via upload

---
+++ iv-checker/sw.js
@@ -0,0 +1,22 @@
+// 最小構成のService Worker: PWAインストール要件+基本キャッシュ
+const CACHE='ivc-v1';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).catch(()=>{}));
+  self.skipWaiting();
+});
+self.addEventListener('activate',e=>{
+  e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+  self.clients.claim();
+});
+// ネットワーク優先(更新反映を優先)、オフライン時のみキャッシュ
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET') return;
+  e.respondWith(
+    fetch(e.request).then(r=>{
+      const cp=r.clone();
+      caches.open(CACHE).then(c=>c.put(e.request,cp)).catch(()=>{});
+      return r;
+    }).catch(()=>caches.match(e.request))
+  );
+});
